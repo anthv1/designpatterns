c485c84fffa358314d5b77ee087df2b624afa75d
Anth <anthonyyang577@gmail.com>
2023-11-29 14:14:51 +1100

    factory patterns

---
+++ factoryP/buttonsFactory/Button.java
@@ -0,0 +1,14 @@
+package buttonsFactory;
+
+// PRODUCT INTERFACE
+public interface Button {
+	
+	public void setLabel(String label);
+	public String getLabel();
+
+	public void setBgColour(String colour);
+	public String getBgColour();
+	
+	public void click();
+
+}
